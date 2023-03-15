19d37928bc8cbe826a19bde8016bb1ab70b80e61
JOBERT Samuel <samuel.jobert@axa.fr>
2023-03-15 08:35:13 +0100

    Correct assignments in @examples (<-)

--- R/keep_with_next.R
+++ R/keep_with_next.R
@@ -9,20 +9,20 @@
 #' Can be a logical vector of same length as number of rows of the table
 #' @param value Logical. TRUE will keep the row attached to the following row's first paragraph in case of page break.
 #' @examples
-#' df = data.frame(
+#' df <- data.frame(
 #' 	"Items" = c(rep("Item 1", 10), rep("Item 2", 15), rep("Item 3", 25), rep("Item 4", 5), rep("Item 5", 25)),
 #' 	 "var_bidon" = seq_len(10 + 15 + 25 + 5 + 25)
 #' )
-#' ft = flextable(df)
+#' ft <- flextable(df)
 #' 
 #' # Set all rows keep_with_next to TRUE
-#' ft = ft %>% set_keep_with_next(rows = NULL, value = TRUE)
+#' ft <- ft %>% set_keep_with_next(rows = NULL, value = TRUE)
 #' # Select rows after which the break can append (all lines of an item have to be on the same page)
-#' ft = ft %>% set_keep_with_next(rows = c(10, 10+15, 10+15+25, 10+15+25+5), value = FALSE)
+#' ft <- ft %>% set_keep_with_next(rows = c(10, 10+15, 10+15+25, 10+15+25+5), value = FALSE)
 #' 
-#' doc = read_docx()
-#' doc = doc %>% body_add_par("Insert lines here !")
-#' doc = doc %>% body_add_flextable(ft, keepnext = TRUE, split = FALSE)
+#' doc <- read_docx()
+#' doc <- doc %>% body_add_par("Insert lines here !")
+#' doc <- doc %>% body_add_flextable(ft, keepnext = TRUE, split = FALSE)
 #' print(doc, target = "mon_doc.docx")
 
 set_keep_with_next <- function(x, rows = NULL, value = TRUE) {
